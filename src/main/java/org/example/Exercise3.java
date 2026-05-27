a6a990eceaca35861196bbcd2df23b7e22a83a3d
Anastasiya Huk <a.guk@syberry.com>
2026-05-27 12:37:52 +0200

    Made exercise3

---
+++ src/main/java/org/example/Exercise3.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+ /*
+ Максимум из двух
+  */
+public class Exercise3 {
+    static void main() {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int number1 = console.nextInt();
+
+        Scanner console2 = new Scanner(System.in);
+        System.out.print("Enter number 2: ");
+        int number2 = console2.nextInt();
+
+        if (number1 > number2) {
+            System.out.print(number1);
+        } else {
+            System.out.print(number2);
+        }
+    }
+}
