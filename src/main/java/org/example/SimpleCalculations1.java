97f6eba2c9087e34018a121234988fda4d5dd098
Anastasiya Huk <a.guk@syberry.com>
2026-05-28 18:21:06 +0200

    Made SimpleCalculations1

---
+++ src/main/java/org/example/SimpleCalculations1.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+/*
+Sum of Two Numbers
+ */
+
+public class SimpleCalculations1 {
+    static void main() {
+        Scanner console = new Scanner(System.in);
+
+        System.out.print("Enter number 1: ");
+        int number1 = console.nextInt();
+
+        System.out.print("Enter number 2: ");
+        int number2 = console.nextInt();
+
+        System.out.print("Sum: " + (number1 + number2));
+    }
+}
