59c840c72045acf76e75c916caa82345d6804067
Anastasiya Huk <a.guk@syberry.com>
2026-05-28 14:28:30 +0200

    Made StringValidation3

---
+++ src/main/java/org/example/StringValidation3.java
@@ -0,0 +1,24 @@
+package org.example;
+
+/*
+Compare Two Strings
+ */
+
+import java.util.Scanner;
+
+public class StringValidation3 {
+    static void main() {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter field 1: ");
+        String field1 = console.nextLine();
+
+        System.out.print("Enter field 2: ");
+        String field2 = console.nextLine();
+
+        if (field1.equals(field2)){
+            System.out.print("Strings are equal");
+        }else{
+            System.out.print("Strings are not equal");
+        }
+    }
+}
