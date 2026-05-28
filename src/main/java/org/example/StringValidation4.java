b08ffdd94f19677fdf13721f70fa765086e77c64
Anastasiya Huk <a.guk@syberry.com>
2026-05-28 14:40:52 +0200

    Made StringValidation4

---
+++ src/main/java/org/example/StringValidation4.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/*
+Username Length Check
+ */
+
+import java.util.Scanner;
+
+public class StringValidation4 {
+    static void main() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String username = scan.nextLine();
+
+        if (username.length() >= 3 && username.length() <= 15){
+            System.out.print("Valid username");
+        }else{
+            System.out.print("Invalid username");
+        }
+    }
+}
