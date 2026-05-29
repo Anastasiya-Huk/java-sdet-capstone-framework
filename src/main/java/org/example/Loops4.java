b4b0f6300fbd25b4685ad45a2e6d83b569a7e2b2
Anastasiya Huk <a.guk@syberry.com>
2026-05-29 18:32:34 +0200

    Made Loops4

---
+++ src/main/java/org/example/Loops4.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/*
+Countdown
+ */
+
+public class Loops4 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Happy New Year!");
+    }
+}
