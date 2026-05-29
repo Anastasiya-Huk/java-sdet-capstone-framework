d822f562d3fba259eb3841f6276828ba9a0e322e
Anastasiya Huk <a.guk@syberry.com>
2026-05-29 13:45:39 +0200

    Made SimpleCalculations4

---
+++ src/main/java/org/example/SimpleCalculations4.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/*
+Checking the weather outside
+ */
+
+public class SimpleCalculations4 {
+    public static void main(String[] args) {
+        int currentTemperature = -5;
+
+        if (currentTemperature < 0) {
+            System.out.println("Оденьтесь потеплее, на улице мороз!");
+        } else {
+            System.out.println("Можно идти в легкой одежде!");
+        }
+    }
+}
