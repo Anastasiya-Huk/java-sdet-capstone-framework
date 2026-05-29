ba0c81504072c0062150025d68177b34812032fd
Anastasiya Huk <a.guk@syberry.com>
2026-05-29 17:48:58 +0200

    Made Loops2

---
+++ src/main/java/org/example/Loops2.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/*
+Unlocking a phone
+ */
+
+import java.util.Scanner;
+
+public class Loops2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String input = "";
+
+        while (!input.equals("java")) {
+            System.out.println("Enter password: ");
+            input = console.next();
+        }
+        System.out.println("The phone is unlocked!");
+    }
+}
