645d93862d4f106586d91c0d12fa329ca62654dd
Anastasiya Huk <a.guk@syberry.com>
2026-05-29 13:38:40 +0200

    Made SimpleCalculations3

---
+++ src/main/java/org/example/SimpleCalculations3.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/*
+Buying a movie ticket
+ */
+
+public class SimpleCalculations3 {
+    public static void main(String[] args) {
+        int visitorAge = 17;
+        if (visitorAge < 18){
+            System.out.print("Доступ запрещен");
+        }else{
+            System.out.print("Добро пожаловать!");
+        }
+    }
+}
