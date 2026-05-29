0e6f67d2499d375802b8537a6d52f10061a0105a
Anastasiya Huk <a.guk@syberry.com>
2026-05-29 17:24:53 +0200

    Made Loops1

---
+++ src/main/java/org/example/Loops1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/*
+Countdown before the start
+ */
+
+public class Loops1 {
+    public static void main(String[] args) {
+        int number = 3;
+
+        while (number > 0) {
+            System.out.println(number);
+            number--;
+        }
+        System.out.println("Старт!");
+    }
+}
