69e2123a44ecb7c59717acd23916f8bdedd48d6d
Anastasiya Huk <a.guk@syberry.com>
2026-05-29 15:59:58 +0200

    Made ConditionalLogic6

---
+++ src/main/java/org/example/ConditionalLogic6.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/*
+Conditions for obtaining a bank loan
+ */
+
+public class ConditionalLogic6 {
+    public static void main(String[] args) {
+        int yourAge = 25;
+        boolean hasJob = true;
+        boolean hasCreditHistory = false;
+        boolean hasGuarantor = true;
+
+        boolean credit1 = (yourAge > 21 && hasJob) || (hasCreditHistory && hasGuarantor);
+
+        boolean credit2 = (yourAge > 21) && (hasJob || hasCreditHistory) && hasGuarantor;
+
+        System.out.println(credit1 + " " + credit2);
+    }
+}
