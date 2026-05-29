8c2288d4dc3f081efa293160b85af50e0ee3b503
Anastasiya Huk <a.guk@syberry.com>
2026-05-29 18:02:53 +0200

    Made Loops3

---
+++ src/main/java/org/example/Loops3.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/*
+Display of seats
+ */
+
+public class Loops3 {
+    public static void main(String[] args) {
+        int number = 2;
+
+        while (number <= 10) {
+            System.out.println(number);
+            number +=2;}
+    }
+}
