838ed113fa78e780e3ceb0743a407170d147c7c3
Anastasiya Huk <a.guk@syberry.com>
2026-05-29 19:02:34 +0200

    Made Loops5

---
+++ src/main/java/org/example/Loops5.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/*
+Coordinates of objects
+ */
+
+public class Loops5 {
+    public static void main(String[] args) {
+        for (int a = 1, b = 5; a <= 5 && b >= 1; a++, b--) {
+            System.out.println(a + " " + b);
+        }
+    }
+}
