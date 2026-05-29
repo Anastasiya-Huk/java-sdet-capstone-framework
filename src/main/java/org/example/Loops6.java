47d9308df2cd9f8ed62dc6494f63c57965cd2b40
Anastasiya Huk <a.guk@syberry.com>
2026-05-29 19:12:11 +0200

    Made Loops6

---
+++ src/main/java/org/example/Loops6.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/*
+Downloading the application
+ */
+
+public class Loops6 {
+    public static void main(String[] args) {
+        int gameVersion = 10;
+
+        do {
+            System.out.println("Loading...");
+            System.out.println(gameVersion);
+        } while (gameVersion != 10);
+    }
+}
