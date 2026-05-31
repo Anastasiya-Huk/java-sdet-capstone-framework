9976419226297b1a43e17d25f9b17e30150da055
Anastasiya Huk <a.guk@syberry.com>
2026-05-31 11:47:52 +0200

    Made Loops8

---
+++ src/main/java/org/example/Loops8.java
@@ -0,0 +1,25 @@
+package org.example;
+
+/*
+Menu with choice
+ */
+
+import java.util.Scanner;
+
+public class Loops8 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String menu = "Menu:\n1. Display greeting\n2. Exit";
+        int number;
+
+        do {
+            System.out.println(menu);
+            number = console.nextInt();
+
+            if (number == 1) {
+                System.out.println("Hello!");
+            }
+
+        } while (number != 2);
+    }
+}
