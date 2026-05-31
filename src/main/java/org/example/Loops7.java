0667031128e8d41fe35d71621b1f6596aedb5970
Anastasiya Huk <a.guk@syberry.com>
2026-05-31 11:26:35 +0200

    Made Loops7

---
+++ src/main/java/org/example/Loops7.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/*
+Entering a PIN code
+ */
+
+import java.util.Scanner;
+
+public class Loops7 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int pin;
+
+        do {
+            System.out.println("Enter PIN: ");
+            pin = console.nextInt();
+
+        } while (pin < 1000 || pin > 9999);
+
+        System.out.println("PIN accepted");
+    }
+}
