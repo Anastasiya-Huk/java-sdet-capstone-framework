46887aeaba2938068144f2aab61677515e92ea7d
Anastasiya Huk <a.guk@syberry.com>
2026-05-31 12:00:37 +0200

    Made Loops9

---
+++ src/main/java/org/example/Loops9.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/*
+Checking the password length
+ */
+
+import java.util.Scanner;
+
+public class Loops9 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String password;
+
+        do {
+            System.out.println("Enter your password (at least 6 characters): ");
+            password = console.nextLine();
+
+        } while (password.length() < 6);
+
+        System.out.println("The password is secure.");
+    }
+}
