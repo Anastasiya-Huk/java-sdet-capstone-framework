fcafb993d19f2b9f457400830e663a45d880ec9a
Anastasiya Huk <a.guk@syberry.com>
2026-05-31 14:59:55 +0200

    Made Loops10

---
+++ src/main/java/org/example/Loops10.java
@@ -0,0 +1,25 @@
+package org.example;
+
+/*
+Sales summation
+ */
+
+import java.util.Scanner;
+
+public class Loops10 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Enter the sales amount: ");
+            int cost = console.nextInt();
+
+            if (cost < 0) {
+                break;
+            }
+            sum += cost;
+        }
+        System.out.println("Total sales amount: " + sum);
+    }
+}
