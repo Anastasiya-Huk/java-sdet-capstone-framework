a42ef85c5e13886b18a458c180cafb033f40d9be
Anastasiya Huk <a.guk@syberry.com>
2026-06-01 18:24:56 +0200

    Made Arrays5

---
+++ src/main/java/org/example/task_1139737/Arrays5.java
@@ -0,0 +1,21 @@
+package org.example.task_1139737;
+
+/*
+Displaying array elements in reverse order
+ */
+
+import java.util.Scanner;
+
+public class Arrays5 {
+    public static void main(String[] args) {
+        String[] favoriteMovies = new String[3];
+        Scanner console = new Scanner(System.in);
+
+        for (int i = 0; i < favoriteMovies.length; i++){
+            favoriteMovies[i] = console.nextLine();
+        }
+        for (int i = favoriteMovies.length - 1; i >= 0; i--){
+            System.out.println(favoriteMovies[i]);
+        }
+    }
+}
