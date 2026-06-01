87342df950e98515f2c88ee0fd2736e2602ee23c
Anastasiya Huk <a.guk@syberry.com>
2026-06-01 15:40:32 +0200

    Made Arrays2

--- src/main/java/org/example/task_1139737/Arrays1.java
+++ src/main/java/org/example/task_1139737/Arrays1.java
@@ -6,8 +6,8 @@ Creating an array
 
 public class Arrays1 {
     public static void main(String[] args) {
-        int [] inventorySlots = new int [5];
+        int[] inventorySlots = new int[5];
         inventorySlots [0] = 7;
-        System.out.println(inventorySlots [0]);
+        System.out.println(inventorySlots[0]);
     }
 }
---
+++ src/main/java/org/example/task_1139737/Arrays2.java
@@ -0,0 +1,16 @@
+package org.example.task_1139737;
+
+/*
+Counting the number of elements in an array
+ */
+
+public class Arrays2 {
+    public static void main(String[] args) {
+        String[] favoriteLanguages = new String[3];
+        favoriteLanguages[0] = "Java";
+        favoriteLanguages[1] = "Java";
+        favoriteLanguages[2] = "Java";
+        int n = favoriteLanguages.length;
+        System.out.println(n);
+    }
+}
