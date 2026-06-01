c49bc56375412d26a27dc375c8f6e9560acfb15b
Anastasiya Huk <a.guk@syberry.com>
2026-06-01 18:02:41 +0200

    Made Arrays4

---
+++ src/main/java/org/example/task_1139737/Arrays4.java
@@ -0,0 +1,14 @@
+package org.example.task_1139737;
+
+/*
+Displaying array elements
+ */
+
+public class Arrays4 {
+    public static void main(String[] args) {
+        int[] gameHighScores = {10, 20, 30, 40, 50};
+        for (int i = 0; i < gameHighScores.length; i++){
+            System.out.println(gameHighScores[i]);
+        }
+    }
+}
