2bec75aad7d88d2358d7213307b8c1e5e115f0c2
Anastasiya Huk <a.guk@syberry.com>
2026-06-02 20:13:28 +0200

    Made Arrays12

---
+++ src/main/java/org/example/task_1139737/Arrays12.java
@@ -0,0 +1,15 @@
+package org.example.task_1139737;
+
+/*
+Copying elements from an array
+ */
+
+import java.util.Arrays;
+
+public class Arrays12 {
+    public static void main(String[] args) {
+        int[] dailyTemperatures = {10, 20, 30, 40, 50, 60, 70};
+        int[] arr = Arrays.copyOfRange(dailyTemperatures, 2, 5);
+        System.out.println(Arrays.toString(arr));
+    }
+}
