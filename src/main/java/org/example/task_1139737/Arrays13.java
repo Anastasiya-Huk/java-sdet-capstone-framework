a44e7551800c5cdeedfaae7c670b3e166ffe5ca8
Anastasiya Huk <a.guk@syberry.com>
2026-06-02 20:19:30 +0200

    Made Arrays13

---
+++ src/main/java/org/example/task_1139737/Arrays13.java
@@ -0,0 +1,15 @@
+package org.example.task_1139737;
+
+/*
+Comparing the contents of arrays for equality
+ */
+
+import java.util.Arrays;
+
+public class Arrays13 {
+    public static void main(String[] args) {
+        int[] mainCode = {3, 6, 9, 12};
+        int[] backupCode = {3, 6, 9, 12};
+        System.out.println(Arrays.equals(mainCode, backupCode));
+    }
+}
