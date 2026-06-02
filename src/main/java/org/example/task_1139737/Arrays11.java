63251fcfb8d31e31c3f214f1df4af8ee89aea614
Anastasiya Huk <a.guk@syberry.com>
2026-06-02 16:10:01 +0200

    Made Arrays11

---
+++ src/main/java/org/example/task_1139737/Arrays11.java
@@ -0,0 +1,15 @@
+package org.example.task_1139737;
+
+/*
+Filling an array with a value
+ */
+
+import java.util.Arrays;
+
+public class Arrays11 {
+    public static void main(String[] args) {
+        String[] warehouseSlots = new String[6];
+        Arrays.fill(warehouseSlots, "Empty");
+        System.out.println(Arrays.toString(warehouseSlots));
+    }
+}
