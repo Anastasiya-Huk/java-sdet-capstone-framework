3b7847b588214243602a7198e97a840de3fe220a
Anastasiya Huk <a.guk@syberry.com>
2026-06-03 17:43:58 +0200

    Made Methods1

---
+++ src/main/java/org/example/task_1139737/Methods1.java
@@ -0,0 +1,16 @@
+package org.example.task_1139737;
+
+/*
+Method call
+ */
+
+public class Methods1 {
+
+  public static void welcomeNewUser(String userName) {
+    System.out.println("Hello, " + userName);
+  }
+
+  public static void main(String[] args) {
+    welcomeNewUser("Anna");
+  }
+}
