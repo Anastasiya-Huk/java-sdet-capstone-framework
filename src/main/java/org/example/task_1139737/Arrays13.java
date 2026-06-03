3c5050f3cb84da6df8950626702c4e3fa6a102e8
Anastasiya Huk <a.guk@syberry.com>
2026-06-03 14:05:10 +0200

    Added design

--- src/main/java/org/example/task_1139737/Arrays13.java
+++ src/main/java/org/example/task_1139737/Arrays13.java
@@ -7,9 +7,10 @@ Comparing the contents of arrays for equality
 import java.util.Arrays;
 
 public class Arrays13 {
-    public static void main(String[] args) {
-        int[] mainCode = {3, 6, 9, 12};
-        int[] backupCode = {3, 6, 9, 12};
-        System.out.println(Arrays.equals(mainCode, backupCode));
-    }
+
+  public static void main(String[] args) {
+    int[] mainCode = {3, 6, 9, 12};
+    int[] backupCode = {3, 6, 9, 12};
+    System.out.println(Arrays.equals(mainCode, backupCode));
+  }
 }
