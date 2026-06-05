93358282511d85b17c2c44e6e8cf5570f7f90ee1
Anastasiya Huk <a.guk@syberry.com>
2026-06-05 11:03:22 +0200

    Made Methods4

---
+++ src/main/java/org/example/task_1139737/Methods4.java
@@ -0,0 +1,18 @@
+package org.example.task_1139737;
+
+/*
+The sum of two numbers
+ */
+
+public class Methods4 {
+
+  public static int sum(int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    int result = sum(5, 3);
+    System.out.println(result);
+  }
+}
+
