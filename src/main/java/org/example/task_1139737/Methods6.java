07e929231396dd7da5908eac23957be663ae1b0d
Anastasiya Huk <a.guk@syberry.com>
2026-06-05 11:19:45 +0200

    Made Methods6

---
+++ src/main/java/org/example/task_1139737/Methods6.java
@@ -0,0 +1,16 @@
+package org.example.task_1139737;
+
+/*
+Calculating the length of a string
+ */
+
+public class Methods6 {
+
+  public static int getLength(String text){
+    return text.length();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(getLength("Java"));
+  }
+}
