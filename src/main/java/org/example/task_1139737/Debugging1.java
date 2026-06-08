39e3faadf779766778b6da9fa62285533fe98913
Anastasiya Huk <a.guk@syberry.com>
2026-06-08 12:08:30 +0200

    Made Debugging1

---
+++ src/main/java/org/example/task_1139737/Debugging1.java
@@ -0,0 +1,16 @@
+package org.example.task_1139737;
+
+public class Debugging1 {
+
+  public static void main(String[] args) {
+
+    int sum = 0;
+
+    for (int i = 1; i <= 5; i++) {
+      sum += i;
+    }
+
+    System.out.println(sum);
+  }
+}
+
