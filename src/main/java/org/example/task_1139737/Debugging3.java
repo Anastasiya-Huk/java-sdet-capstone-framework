41fe9bdf752ccb8685d61c11a8f2f10235469593
Anastasiya Huk <a.guk@syberry.com>
2026-06-08 12:19:34 +0200

    Made Debugging3

---
+++ src/main/java/org/example/task_1139737/Debugging3.java
@@ -0,0 +1,17 @@
+package org.example.task_1139737;
+
+/*
+Finding the bug
+ */
+
+public class Debugging3 {
+  public static void main(String[] args) {
+
+    int[] numbers = {1, 2, 3, 4};
+
+    for (int i = 0; i <= numbers.length; i++) {
+      System.out.println(numbers[i]);
+    }
+  }
+
+}
